f4f5fcda98143d9d9aa49710ae5b723ba0ac5ee6
trinhan2103la <trinhan212003la@gmail.com>
2024-08-01 16:53:41 +0700

    [pdate] scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import Content from "./components/container/content";
 import Header from "./components/header/Header";
+import ScrollToTop from "./components/container/content/comp/ScrollToTop";
 
 function App() {
   return (
-    <>
-      <div>
+    <div className="min-h-[100vh] relative">
+      <div className="fixed top-0 w-full z-10">
         <Header />
       </div>
-      <div className="min-h-[100vh] bg-light">
+      <div className="bg-light pt-[50px]">
         <Content />
       </div>
-    </>
+      <ScrollToTop />
+    </div>
   );
 }
 
